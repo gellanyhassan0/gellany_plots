c82b8708a08a614b3674126e8a039106906f598d
gellanyhassan0 <36517065+gellanyhassan0@users.noreply.github.com>
2022-05-30 08:09:37 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+with open('requirements.txt','r') as fr:
+    requires = fr.read().split('\n')
 
 setuptools.setup(
     name="gellany_plots",
@@ -23,4 +25,5 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+    install_requires=requires,
 )
